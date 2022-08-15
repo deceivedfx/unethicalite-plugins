64ebf1e89efa02997d4eb526f8598cd23dd05846
dxcvr <onlytainted@gmail.com>
2022-08-14 22:30:56 -0500

    add test plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,3 +44,4 @@ for (project in rootProject.children) {
         require(buildFile.isFile) { "Project '${project.path} must have a $buildFile build script" }
     }
 }
+include("test-plugin")
---
+++ test-plugin/test-plugin.gradle.kts
@@ -0,0 +1,18 @@
+version = "0.0.3"
+
+project.extra["PluginName"] = "Test plugin v2"
+project.extra["PluginDescription"] = "Plugin for testing experimental features"
+
+tasks {
+    jar {
+        manifest {
+            attributes(mapOf(
+                "Plugin-Version" to project.version,
+                "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                "Plugin-Provider" to project.extra["PluginProvider"],
+                "Plugin-Description" to project.extra["PluginDescription"],
+                "Plugin-License" to project.extra["PluginLicense"]
+            ))
+        }
+    }
+}
